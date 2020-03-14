8244af98d4535ce985439f26f0b1c2de31306b4c
tomimat <tomas.mateo@bits0.com>
2020-03-14 15:49:22 -0300

    -Adding build to git.

---
+++ web/build/precache-manifest.83ff102b4bc8d97361ac9c256ff9c934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e41b26ab46f7142e71e3772c3307341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a835248f7ab6da9d04",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "3776c66cd9dc878dee01",
+    "url": "/static/css/main.c640c832.chunk.css"
+  },
+  {
+    "revision": "a5a835248f7ab6da9d04",
+    "url": "/static/js/2.9e42448c.chunk.js"
+  },
+  {
+    "revision": "3776c66cd9dc878dee01",
+    "url": "/static/js/main.5eb73cbc.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
